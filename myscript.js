2a900911d4586e45b7dc2deb841e97c73c6e4823
Annika18 <klannika@gmail.com>
2017-07-17 15:29:50 -0500

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,12 @@
+
+
+
+
+function longIntro() {
+  if (document.getElementById("introPlus").style.visibility == "visible") {
+    document.getElementById("introPlus").style.visibility="hidden";
+  }
+  else {
+    document.getElementById("introPlus").style.visibility="visible";
+  }
+}
